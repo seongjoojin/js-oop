d643d1b4be4810ebcdf29af47f4c1b8519c011f3
Seongjoo Jin <evanjin8@gmail.com>
2020-02-29 23:15:25 +0900

    this

---
+++ this.js
@@ -0,0 +1,11 @@
+const kim = {
+  name: 'kim',
+  first: 10,
+  second: 20,
+  sum: function() {
+    return this.first + this.second;
+  }
+};
+
+// console.log('kim.sum(kim.first, kim.second)', kim.sum(kim.first, kim.second));
+console.log('kim.sum(kim.first, kim.second)', kim.sum());

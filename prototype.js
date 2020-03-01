8239e316367314790edda2f193f001f54aa2a3da
Seongjoo Jin <evanjin8@gmail.com>
2020-03-01 09:30:33 +0900

    prototype

---
+++ prototype.js
@@ -0,0 +1,18 @@
+function Person(name, first, second) {
+  this.name = name;
+  this.first = first;
+  this.second = second;
+}
+
+Person.prototype.sum = function() {
+  return `prototype : ${this.first + this.second}`;
+};
+
+const kim = new Person('kim', 10, 20);
+kim.sum = function() {
+  return `this : ${this.first + this.second}`;
+};
+const lee = new Person('lee', 10, 10);
+
+console.log('kim.sum()', kim.sum());
+console.log('lee.sum()', lee.sum());

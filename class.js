d76685a4a9689f28131d55708f1874d501636636
Seongjoo Jin <evanjin8@gmail.com>
2020-03-01 11:08:14 +0900

    super

--- class.js
+++ class.js
@@ -11,11 +11,18 @@ class Person {
 }
 
 class PersonPlus extends Person {
+  constructor(name, first, second, third) {
+    super(name, first, second);
+    this.third = third;
+  }
+  sum() {
+    return super.sum() + this.third;
+  }
   avg() {
-    return (this.first + this.second) / 2;
+    return (this.first + this.second + this.third) / 3;
   }
 }
 
-const kim = new PersonPlus('kim', 10, 20);
+const kim = new PersonPlus('kim', 10, 20, 30);
 console.log('kim.sum()', kim.sum());
 console.log('kim.avg()', kim.avg());
